9296da3b0dfcffb0e79fe3bbb6afc34a8f82989f
caidchen <caidchen@126.com>
2017-04-16 14:19:18 +0800

    应用主函数绑定Mapper目录

--- qt-trading-web/src/main/java/com/qtdbp/trading/WebApplication.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/WebApplication.java
@@ -1,9 +1,10 @@
 package com.qtdbp.trading;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,7 @@ import org.springframework.http.HttpStatus;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@MapperScan(basePackages = "com.qtdbp.trading.mapper")
 public class WebApplication {
 
     public static void main(String[] args) {
