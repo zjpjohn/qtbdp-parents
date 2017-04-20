b79ff5a0e75cea87baa8450bbe1dd1280228f6cb
caidchen <caidchen@126.com>
2017-04-20 14:35:51 +0800

    个人中心个人用户成为服务商

--- qt-trading-web/src/main/java/com/qtdbp/trading/mapper/DataInstitutionInfoMapper.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/mapper/DataInstitutionInfoMapper.java
@@ -1,15 +1,34 @@
 package com.qtdbp.trading.mapper;
 
 import com.qtdbp.trading.model.DataInstitutionInfoModel;
-import com.qtdbp.trading.utils.BaseMapper;
+import com.qtdbp.trading.model.DataInstitutionTypeRelationModel;
 
 import java.util.List;
 
 /**
+ * 数据服务商数据库CRUD操作
+ *
  * Created by liyang on 2017/4/19.
  */
-public interface DataInstitutionInfoMapper extends BaseMapper<DataInstitutionInfoModel> {
+public interface DataInstitutionInfoMapper {
 
-    //分页查询服务商信息和类型
-    List<DataInstitutionInfoModel> selectInstitutionAndType(DataInstitutionInfoModel infoModel);
+    /**
+     * 分页查询服务商信息和类型
+     * @param dm
+     * @return
+     */
+    List<DataInstitutionInfoModel> selectInstitutionAndType(DataInstitutionInfoModel dm);
+
+    /**
+     * 插入服务商
+     * @param institutionInfoModel
+     * @return 服务商ID
+     */
+    int insertInstitution(DataInstitutionInfoModel institutionInfoModel);
+
+    /**
+     * 插入服务商数据类型关系数据
+     * @param relationModels
+     */
+    void insertInstitutionTypeRelation(List<DataInstitutionTypeRelationModel> relationModels);
 }
