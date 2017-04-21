c7e0b0f4df3f8c0e639c2f0be55ec52a597acd16
caidchen <caidchen@126.com>
2017-04-21 12:37:20 +0800

    数据众包查询接口

--- qt-trading-web/src/main/java/com/qtdbp/trading/api/UserCenterApi.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/api/UserCenterApi.java
@@ -3,25 +3,15 @@ package com.qtdbp.trading.api;
 import com.github.pagehelper.PageInfo;
 import com.qtdbp.trading.constants.ApiConstants;
 import com.qtdbp.trading.exception.GlobalException;
-import com.qtdbp.trading.model.DataAuthorizeOrderModel;
-import com.qtdbp.trading.model.DataBuyInfoModel;
-import com.qtdbp.trading.model.DataSosInfoModel;
-import com.qtdbp.trading.model.DataTransactionOrderModel;
-import com.qtdbp.trading.service.DataAuthorizeOrderService;
-import com.qtdbp.trading.service.DataBuyInfoService;
-import com.qtdbp.trading.service.DataSosInfoService;
-import com.qtdbp.trading.service.DataTransactionOrderService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import com.qtdbp.trading.model.*;
+import com.qtdbp.trading.service.*;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -128,8 +118,8 @@ public class UserCenterApi {
 
     @ApiOperation(value="需求订单数据接口，分页获取")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId", value = "用户ID（如：1）", dataType = "Integer", required = true, paramType = ApiConstants.PARAM_TYPE_QUERY),
-            @ApiImplicitParam(name = "productType", value = "产品类型（1数据众包、2方案召集）", dataType = "Integer", required = true, paramType = ApiConstants.PARAM_TYPE_QUERY),
+            @ApiImplicitParam(name = "userId", value = "用户ID（如：1）", dataType = "Integer", paramType = ApiConstants.PARAM_TYPE_QUERY),
+            @ApiImplicitParam(name = "productType", value = "产品类型（1数据众包、2方案召集）", dataType = "Integer", paramType = ApiConstants.PARAM_TYPE_QUERY),
             @ApiImplicitParam(name = "orderState", value = "订单状态（1待支付、2已撤销、3已支付）,不传表示所有", dataType = "Integer", paramType = ApiConstants.PARAM_TYPE_QUERY),
             @ApiImplicitParam(name = "page", value = "当前页（如：1）", defaultValue = "1", dataType = "Integer", required = true, paramType = ApiConstants.PARAM_TYPE_QUERY),
             @ApiImplicitParam(name = "rows", value = "每页显示记录数（如：10）", defaultValue = "10", dataType = "Integer", required = true, paramType = ApiConstants.PARAM_TYPE_QUERY)
@@ -167,7 +157,7 @@ public class UserCenterApi {
     })
     @ResponseBody
     @RequestMapping(value = "/sosInfos", method = RequestMethod.GET)
-    public ModelMap loadDataSosInfos(DataSosInfoModel sosInfoModel) throws GlobalException {
+    public String loadDataSosInfos(DataSosInfoModel sosInfoModel) throws GlobalException {
 
         ModelMap map = new ModelMap() ;
         // 设置默认每页显示记录数
@@ -182,7 +172,7 @@ public class UserCenterApi {
             throw new GlobalException(e.getMessage()) ;
         }
 
-        return map ;
+        return map.toString() ;
     }
 
     //===================================================================
