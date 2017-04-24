a789f7756d2fe642c9dd33b9a632159818252b11
hlq <252121238@qq.com>
2017-04-24 20:52:57 +0800

    截止时间  运算

---
+++ qt-trading-web/src/main/resources/static/tradecenter/js/timeFormat.js
@@ -0,0 +1,42 @@
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+function getDateDiff(dateTimeStamp){
+    var minute = 1000 * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var halfamonth = day * 15;
+    var month = day * 30;
+    var now = new Date().getTime();
+    var diffValue =dateTimeStamp - now ;
+    if(diffValue < 0){return result="已截止";}
+    var monthC =diffValue/month;
+    var weekC =diffValue/(7*day);
+    var dayC =diffValue/day;
+    var hourC =diffValue/hour;
+    var minC =diffValue/minute;
+    if(dayC>=1) {
+        result=""+ new Date(dateTimeStamp).Format("yyyy-MM-dd")+"截止";
+    }
+    else if(hourC>=1) {
+        result=""+ parseInt(hourC) +"小时后截止";
+    }
+    else if(minC>=1) {
+        result=""+ parseInt(minC) +"分钟后截止";
+    }else {
+        result="即将截止";
+    }
+    return result;
+}
