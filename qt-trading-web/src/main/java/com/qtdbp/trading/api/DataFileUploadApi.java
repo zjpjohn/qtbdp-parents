a138b29483eb9818e2f2a3fd91a1bb32f0657b70
caidchen <caidchen@126.com>
2017-04-25 18:43:48 +0800

    支持图片上传到阿里云Oss接口、支持文件上传FastDfs接口

--- qt-trading-web/src/main/java/com/qtdbp/trading/api/DataFileUploadApi.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/api/DataFileUploadApi.java
@@ -3,9 +3,10 @@ package com.qtdbp.trading.api;
 
 import com.qtd.utils.OssUpload;
 import com.qtdbp.trading.exception.GlobalException;
+import com.qtdbp.trading.service.FdfsUploadService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,29 +26,31 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping(value = "/api/upload")
 public class DataFileUploadApi {
 
+    @Autowired
+    private FdfsUploadService uploadService ;
 
     //===================================================================
     // 图片上传接口-支持阿里云Oss服务
     //===================================================================
 
-    @ApiOperation(value = "上传图片接口")
-    @ApiParam(name = "file", value = "图片内容", required = true)
+    @ApiOperation(value = "上传图片接口，支持jpg/jpeg、gif、png")
     @RequestMapping(value = "/img", method = RequestMethod.POST)
-    public ModelMap imgUpload(@RequestParam MultipartFile file) throws GlobalException {
+    public ModelMap imgUpload(@RequestParam MultipartFile img) throws GlobalException {
 
         ModelMap map = new ModelMap();
         boolean isSuccess = false ;
         String imgUrl ;
         try {
 
-            imgUrl = OssUpload.uploadFileBytes(file.getBytes(), file.getContentType()) ;
+            if(img == null) throw new GlobalException("图片不存在，请先上传图片") ;
+
+            imgUrl = OssUpload.uploadFileBytes(img.getBytes(), img.getContentType()) ;
             if(imgUrl != null) {
                 imgUrl = OssUpload.imagepath +"/"+ imgUrl ;
 
                 isSuccess = true ;
             }
         } catch (Exception e) {
-            e.printStackTrace();
             throw new GlobalException(e.getMessage()) ;
         }
 
@@ -57,4 +60,34 @@ public class DataFileUploadApi {
         return map;
     }
 
+    //===================================================================
+    // 文件上传接口-FastDFS
+    //===================================================================
+
+    @ApiOperation(value = "上传文件接口，格式：xsl、doc、pdf等")
+    @RequestMapping(value = "/file", method = RequestMethod.POST)
+    public ModelMap fileUpload(@RequestParam MultipartFile file) throws GlobalException {
+
+        ModelMap map = new ModelMap();
+        boolean isSuccess = false ;
+        String fileUrl ;
+        try {
+
+            if(file == null) throw new GlobalException("文件不存在，请先上传文件") ;
+
+            fileUrl = uploadService.uploadFile(file) ;
+
+            if(fileUrl != null) {
+                isSuccess = true ;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new GlobalException(e.getMessage()) ;
+        }
+
+        map.put("success", isSuccess) ;
+        map.put("file", fileUrl) ;
+
+        return map;
+    }
 }
