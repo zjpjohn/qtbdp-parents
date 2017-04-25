9dcb4d7b7a34c4979d46061734811b508056503e
liyang <18758009334@163.com>
2017-04-25 10:00:11 +0800

    添加关于我们控制类

---
+++ qt-trading-web/src/main/java/com/qtdbp/trading/controller/AboutController.java
@@ -0,0 +1,22 @@
+package com.qtdbp.trading.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 关于我们页面控制类
+ * Created by liyang on 2017/4/24.
+ */
+@Controller
+public class AboutController {
+
+    private static final String  PAGE_ABOUT = "about";
+
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public String loadAboutUs(){
+  //      ModelAndView modelAndView = new ModelAndView(PAGE_ABOUT);
+        return PAGE_ABOUT;
+    }
+}
