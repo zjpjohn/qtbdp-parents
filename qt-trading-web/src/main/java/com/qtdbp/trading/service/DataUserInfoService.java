e41c61b284576ac34927a6f66fabbf0541a01b19
hlq <252121238@qq.com>
2017-04-26 19:32:41 +0800

    修复图像地址解析问题

--- qt-trading-web/src/main/java/com/qtdbp/trading/service/DataUserInfoService.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/service/DataUserInfoService.java
@@ -2,12 +2,13 @@ package com.qtdbp.trading.service;
 
 import com.qtdbp.trading.exception.GlobalException;
 import com.qtdbp.trading.mapper.DataUserInfoMapper;
-import com.qtdbp.trading.mapper.SysUserMapper;
 import com.qtdbp.trading.model.DataUserInfoModel;
-import com.qtdbp.trading.service.security.model.SysUser;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * 个人用户服务
@@ -21,9 +22,6 @@ public class DataUserInfoService {
 
     @Autowired
     private DataUserInfoMapper userInfoMapper ;
-
-    @Autowired
-    private SysUserMapper sysUserMapper ;
 
     /**
      * 查询用户基本信息
@@ -47,34 +45,16 @@ public class DataUserInfoService {
 
         if(userInfoModel == null) throw new GlobalException("用户信息数据为空") ;
 
+        try {
+            String head = StringUtils.isEmpty(userInfoModel.getHead()) ? null : URLDecoder.decode(userInfoModel.getHead(), "UTF-8") ;
+            userInfoModel.setHead(head);
+        } catch (UnsupportedEncodingException e) {
+            throw new GlobalException("头像解析出错，请重新操作") ;
+        }
         Integer count = userInfoMapper.updateDataUserInfo(userInfoModel) ;
 
         if(count != null && count > 0) return true ;
 
         return false ;
-    }
-
-    /**
-     * 添加新用户
-     * @param userInfoModel
-     * @return
-     * @throws GlobalException
-     */
-    @Transactional
-    public int insertDataUserInfo(DataUserInfoModel userInfoModel) throws GlobalException {
-
-        if(userInfoModel == null) throw new GlobalException("用户信息数据为空") ;
-
-        SysUser sysUser = sysUserMapper.findUserBySsoId(userInfoModel.getSsoUserId()) ;
-
-        if(sysUser != null) throw new GlobalException("用户已存在无需添加") ;
-
-        // 系统用户在交易平台中没有此用户，则插入新纪录
-        Integer count = userInfoMapper.insertDataUserInfo(userInfoModel);
-
-        int id  = -1 ;
-        if(count != null && count > 0) id = userInfoModel.getId() ;
-
-        return id ;
     }
 }
