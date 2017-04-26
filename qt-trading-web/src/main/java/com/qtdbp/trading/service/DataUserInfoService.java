e00a9a734ffd29feceab8d3df81822114e43d326
caidchen <caidchen@126.com>
2017-04-26 19:40:58 +0800

    被人覆盖了添加用户方法

--- qt-trading-web/src/main/java/com/qtdbp/trading/service/DataUserInfoService.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/service/DataUserInfoService.java
@@ -2,10 +2,13 @@ package com.qtdbp.trading.service;
 
 import com.qtdbp.trading.exception.GlobalException;
 import com.qtdbp.trading.mapper.DataUserInfoMapper;
+import com.qtdbp.trading.mapper.SysUserMapper;
 import com.qtdbp.trading.model.DataUserInfoModel;
+import com.qtdbp.trading.service.security.model.SysUser;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -22,6 +25,9 @@ public class DataUserInfoService {
 
     @Autowired
     private DataUserInfoMapper userInfoMapper ;
+
+    @Autowired
+    private SysUserMapper sysUserMapper ;
 
     /**
      * 查询用户基本信息
@@ -56,5 +62,30 @@ public class DataUserInfoService {
         if(count != null && count > 0) return true ;
 
         return false ;
+    }
+
+
+    /**
+     * 添加新用户
+     * @param userInfoModel
+     * @return
+     * @throws GlobalException
+     */
+    @Transactional
+    public int insertDataUserInfo(DataUserInfoModel userInfoModel) throws GlobalException {
+
+        if(userInfoModel == null) throw new GlobalException("用户信息数据为空") ;
+
+        SysUser sysUser = sysUserMapper.findUserBySsoId(userInfoModel.getSsoUserId()) ;
+
+        if(sysUser != null) throw new GlobalException("用户已存在无需添加") ;
+
+        // 系统用户在交易平台中没有此用户，则插入新纪录
+        Integer count = userInfoMapper.insertDataUserInfo(userInfoModel);
+
+        int id  = -1 ;
+        if(count != null && count > 0) id = userInfoModel.getId() ;
+
+        return id ;
     }
 }
