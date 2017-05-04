4cb6919f70ecc8e7e01ea9b439c1ac2c9599e613
liyang <18758009334@163.com>
2017-05-04 14:25:31 +0800

    修改页面刷新的bug

--- qt-trading-web/src/main/java/com/qtdbp/trading/controller/UserCenterController.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/controller/UserCenterController.java
@@ -1,7 +1,11 @@
 package com.qtdbp.trading.controller;
 
+import com.qtdbp.trading.constants.AppConstants;
 import com.qtdbp.trading.constants.SysRoleContants;
 import com.qtdbp.trading.exception.GlobalException;
+import com.qtdbp.trading.mapper.DataTransactionOrderMapper;
+import com.qtdbp.trading.model.DataTransactionOrderModel;
+import com.qtdbp.trading.service.DataTransactionOrderService;
 import com.qtdbp.trading.service.FdfsFileService;
 import com.qtdbp.trading.service.security.model.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * 个人中心Controller
@@ -24,6 +30,14 @@ public class UserCenterController extends BaseController {
 
     @Autowired
     private FdfsFileService fileService ;
+
+    @Autowired
+    private DataTransactionOrderService orderService ;
+
+    @Autowired
+    private DataTransactionOrderMapper orderMapper ;
+
+    private DataTransactionOrderModel orderModel = null;
 
     /**
      * 数据商城首页
@@ -38,6 +52,12 @@ public class UserCenterController extends BaseController {
     public ModelAndView index() {
 
         ModelAndView result = new ModelAndView(PAGE_USER_CENTER);
+        if(orderModel != null){
+            result.addObject("isOrder", 4);
+            result.addObject("order", orderModel);
+            result.addObject("orderState", orderModel.getOrderState());
+            orderModel = null;
+        }
 
         return result ;
     }
@@ -63,5 +83,79 @@ public class UserCenterController extends BaseController {
         }
 
         return file ;
+    }
+
+    /**
+     * 创建订单并且跳转到个人中心
+     * @param productId
+     * @param productType
+     * @return
+     */
+    @RequestMapping(value = "/getOrderInfoAndGoto/{productId}/{productType}", method = RequestMethod.GET)
+    public ModelAndView getIdAndType(@PathVariable("productId") String productId,
+                                     @PathVariable("productType") String productType) throws GlobalException{
+
+        ModelAndView view = new ModelAndView();
+
+        // 未登陆请先登录
+        SysUser user = getPrincipal() ;
+        if(user == null) {
+            view.setViewName("login");
+            return view;
+        }
+
+        if(productId == null || "0".equals(productId)) throw new GlobalException("此产品不存在，请选择其他产品购买") ;
+        int type = productType != null ? Integer.parseInt(productType) : AppConstants.PRODUCT_TYPE_PACKAGE ;
+        int prodId = Integer.parseInt(productId) ;
+
+        DataTransactionOrderModel dataTransactionOrderModel = new DataTransactionOrderModel();
+        dataTransactionOrderModel.setUserId(user.getId());
+        dataTransactionOrderModel.setProductId(prodId);
+        dataTransactionOrderModel.setProductType((byte)type);
+        view.setViewName("redirect:/usercenter");
+        try {
+            DataTransactionOrderModel result = orderService.insertNewOrder(dataTransactionOrderModel);
+            orderModel = result;
+        } catch (GlobalException e) {
+            e.getMessage();
+        }
+        return view;
+    }
+
+    /**
+     * 获取订单信息并且跳转到订单付款页面
+     * @param productId
+     * @param productType
+     * @return
+     */
+    @RequestMapping(value = "/selectOrderAndPay/{productId}/{productType}", method = RequestMethod.GET)
+    public ModelAndView selectOrderAndPay(@PathVariable("productId") String productId,
+                                          @PathVariable("productType") String productType){
+
+        ModelAndView view = new ModelAndView();
+        // 未登陆请先登录
+        SysUser user = getPrincipal() ;
+        if(user == null) {
+            view.setViewName("login");
+            return view;
+        }
+        DataTransactionOrderModel dataTransactionOrderModel = new DataTransactionOrderModel();
+        int prodId = Integer.parseInt(productId) ;
+        dataTransactionOrderModel.setProductId(prodId);
+        int type = productType != null ? Integer.parseInt(productType) : AppConstants.PRODUCT_TYPE_PACKAGE ;
+        dataTransactionOrderModel.setProductType((byte)type);
+        dataTransactionOrderModel.setUserId(user.getId());
+        List<DataTransactionOrderModel> list = orderMapper.findOrderByUserIdAndProductIdAndType(dataTransactionOrderModel);
+        // 如果当前用户已经购买了此数据包产品，则无需购买
+        if(list != null && !list.isEmpty()) {
+            for(DataTransactionOrderModel order : list){
+                // 只要有一条订单待支付或已支付，则不能购买
+                if(order.getOrderState().intValue() == AppConstants.ORDER_STATE_PAYING) {
+                    orderModel = order;
+                    view.setViewName("redirect:/usercenter");
+                }
+            }
+        }
+        return view;
     }
 }
