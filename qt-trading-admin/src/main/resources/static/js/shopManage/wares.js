d79225297e3f49e872f50fc356137fcdbfdf4169
querySelector <453893380@qq.com>
2017-05-25 09:17:09 +0800

    新增文件大小子文件字段

--- qt-trading-admin/src/main/resources/static/js/shopManage/wares.js
+++ qt-trading-admin/src/main/resources/static/js/shopManage/wares.js
@@ -25,28 +25,30 @@ $(function () {
     $tbody.on('click','.isUsed',function () {
         var that = $(this);
         var id = $(this).val();
-            $.ajax({
-                url: "/api/product/changeState",
-                dataType: "json",
-                type:"get",
-                data:{
-                    id:id
-                },
-                error:function () {
-                    layer.msg("修改产品上下架状态失败",{icon: 5});
-                },
-                success: function (ret) {
-                    if(ret.success === true){
-                        if(that.text() == "上架"){
-                            that.removeClass('blue').addClass('red');
-                            that.text("下架");
-                        }else {
-                            that.removeClass('red').addClass('blue');
-                            that.text("上架");
-                        }
+        $.ajax({
+            url: "/api/product/changeState",
+            dataType: "json",
+            type:"get",
+            data:{
+                id:id
+            },
+            error:function () {
+                layer.msg("修改产品上下架状态失败",{icon: 5});
+            },
+            success: function (ret) {
+                if(ret.success === true){
+                    if(that.text() == "上架"){
+                        that.removeClass('blue').addClass('red');
+                        that.text("下架");
+                        layer.msg("上架成功",{icon: 1});
+                    }else {
+                        that.removeClass('red').addClass('blue');
+                        that.text("上架");
+                        layer.msg("下架成功",{icon: 1});
                     }
                 }
-            })
+            }
+        })
     });
 
 
