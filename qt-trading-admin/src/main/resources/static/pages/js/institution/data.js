48b2c07fcdf1213aa9d691788dba0f532b8bd48f
querySelector <453893380@qq.com>
2017-06-07 19:16:54 +0800

    服务商和定制的列表查询与审核

---
+++ qt-trading-admin/src/main/resources/static/pages/js/institution/data.js
@@ -0,0 +1,59 @@
+$(function () {
+    var $tbody = $('tbody'),
+        $searchBtn = $('#searchBtn'),
+        searchInp = $('#searchInp');
+
+
+    var settings = {
+        container:"#datatable_ajax",
+        url: "/api/product",
+        columns: [
+            { "data": "id" },
+            { "data": "dataTypeProps" },
+            { "data": "designation" },
+            { "data": "dataType" },
+            { "data": "createTime" },
+            {"data":"auditStatus"},
+            { "data": null }
+        ],
+        columnDefs:[{
+            targets: 6,
+            render: function (data, type, row, meta) {
+                var isUsed,
+                    color;
+                if(row.auditStatus == 1){
+                    isUsed = "审核已通过";
+                    color = "red";
+                }else {
+                    isUsed = "审核";
+                    color = "blue";
+                }
+                return '<a href="/dataInfo?id=' + row.id + '" class="btn btn-sm green btn-outline filter-submit revise" data-value="'+ row.id +'">查看</a>' +
+                    '<button class="btn btn-sm '+ color + ' btn-outline filter-cancel isUsed"  data-target="#completeModal"  data-status="'+ row.auditStatus + '" value="'+ row.id +'">'+ isUsed + '</button>';
+            }
+        },
+            { "orderable": false, "targets": 4 }
+        ]
+    };
+
+    Common.ajaxTable(settings);
+
+
+    // //审核操作
+    $tbody.on('click','.isUsed',function () {
+        var id = $(this).val(),
+            status = $(this).data("status");
+
+        if(status == 1){
+            return;
+        }else {
+            Common.Modal("服务商数据审核",id,"/api/institutionV2/audit");
+        }
+    });
+
+
+
+
+
+
+});
