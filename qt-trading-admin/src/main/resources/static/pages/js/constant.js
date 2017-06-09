460541e8f4784cd18354ca67cbad374761f94b2f
liyang <18758009334@163.com>
2017-06-09 13:44:02 +0800

    调试查看详情的显示问题

---
+++ qt-trading-admin/src/main/resources/static/pages/js/constant.js
@@ -0,0 +1,35 @@
+/**
+ * 网站类型
+ * @type {{0: string, 1: string, 2: string, 3: string, 4: string, 5: string, 6: string}}
+ */
+var WEB_TYPE  = {
+    0 : "其他",
+    1 : "社交网站",
+    2 : "电商网站",
+    3 : "金融网站",
+    4 : "生活网站",
+    5 : "行业网站",
+    6 : "新闻网站"
+};
+
+/**
+ *  数据类型
+ * @type {{0: string, 1: string, 2: string}}
+ */
+var DATA_STATUS = {
+    0 : "数据包",
+    1 : "API接口",
+    2 : "数据报告"
+};
+
+/**
+ * 数据来源
+ * @type {{0: string, 1: string, 2: string, 3: string, 4: string}}
+ */
+var DATA_SRC = {
+    0 : "个人",
+    1 : "企业",
+    2 : "政府",
+    3 : "机构",
+    4 : "互联网"
+};
