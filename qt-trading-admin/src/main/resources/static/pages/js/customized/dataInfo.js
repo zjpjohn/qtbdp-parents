c879a18ed93f276efc6df7937ad3d1a4b03efee7
querySelector <453893380@qq.com>
2017-06-12 09:52:01 +0800

    俩个定制详情页的数据类别展示

--- qt-trading-admin/src/main/resources/static/pages/js/customized/dataInfo.js
+++ qt-trading-admin/src/main/resources/static/pages/js/customized/dataInfo.js
@@ -13,7 +13,7 @@ $(function () {
         $('#synopsis').val(data.desc);//描述
         $('#range').val(data.dimension);//采集维度
         $('#scale').val(data.scale);//规模
-        $('#type').val(data.serviceType);//数据类别
+        $('#type').val(Common.typeSelect(dataTypeJson,data.serviceType));//数据类别
         $('#price').val(data.price);//金额
         $('#net').val(data.website);//网站
         $('#time').val(Common._formatedate(data.endTime));//时间
