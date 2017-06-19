d139dc7d47605f621dc7b4acfd9160d0e6f23dda
caid <caidchen@126.com>
2017-06-19 12:27:14 +0800

    修复登陆问题

--- qt-trading-web/src/main/java/com/qtdbp/trading/controller/LoginController.java
+++ qt-trading-web/src/main/java/com/qtdbp/trading/controller/LoginController.java
@@ -1,16 +1,16 @@
 package com.qtdbp.trading.controller;
 
-import com.qtdbp.trading.constants.AppConstants;
 import com.qtdbp.trading.constants.SsoConstants;
 import com.qtdbp.trading.exception.ErrorCode;
 import com.qtdbp.trading.exception.GlobalException;
+import com.qtdbp.trading.mapper.SysUserMapper;
 import com.qtdbp.trading.model.DataUserInfoModel;
 import com.qtdbp.trading.service.DataUserInfoService;
+import com.qtdbp.trading.service.security.model.SysUser;
 import com.qtdbp.trading.utils.Des3;
 import com.qtdbp.trading.utils.Message;
 import com.qtdbp.trading.utils.SsoUrlUtil;
 import net.sf.json.JSONObject;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,7 +25,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URLEncoder;
-import java.util.Map;
 
 /**
  * 用户登录Controller
@@ -51,7 +49,8 @@ public class LoginController {
 
     @Autowired
     private DataUserInfoService userInfoService ;
-
+    @Autowired
+    private SysUserMapper userService;
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
@@ -61,12 +60,6 @@ public class LoginController {
      */
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login() {
-        /*String url = SsoUrlUtil.generateUrl(ssoLoginUrl, ssoCallback) ;
-        logger.info("url: "+url);
-
-        ModelAndView view = new ModelAndView();
-        view.setViewName("redirect:" + url);*/
-
         ModelAndView view = new ModelAndView(PAGE_LOGIN);
 
         return view;
@@ -107,7 +100,28 @@ public class LoginController {
             String tokenObject = stringRedisTemplate.opsForValue().get(descToken) ;
             if(tokenObject != null) {
                 JSONObject ssoUser = JSONObject.fromObject(tokenObject) ;
-                if(ssoUser != null && ssoUser.has("id")) sysUserId = ssoUser.getString("id");
+                if(ssoUser != null && ssoUser.has(SsoConstants.SSO_ID)) {
+                    sysUserId = ssoUser.getString(SsoConstants.SSO_ID);
+
+                    // 首次登陆交易平台，添加本地用户信息
+                    SysUser user = userService.findUserBySsoId(sysUserId) ;
+                    if(user == null) {
+                        DataUserInfoModel localUser = new DataUserInfoModel() ;
+                        // ssoId
+                        localUser.setSsoUserId(sysUserId);
+                        // 账号
+                        if(ssoUser.has(SsoConstants.SSO_USER_NAME))
+                            localUser.setPhone(ssoUser.getString(SsoConstants.SSO_USER_NAME));
+                        // 头像
+                        if(ssoUser.has(SsoConstants.SSO_HEAD))
+                            localUser.setHead(ssoUser.getString(SsoConstants.SSO_HEAD));
+                        // 昵称
+                        if(ssoUser.has(SsoConstants.SSO_NICKNAME))
+                            localUser.setNick(ssoUser.getString(SsoConstants.SSO_NICKNAME));
+                        int id = userInfoService.insertDataUserInfo(localUser) ;
+                        if(id < 0) throw new GlobalException("用户授权失败，请重新授权") ;
+                    }
+                }
             }
 
             message.setSuccess(true);
@@ -128,89 +142,4 @@ public class LoginController {
 
         return map ;
     }
-
-    /**
-     * 用户首次登陆交易平台，跳转至授权页面
-     * @return
-     */
-    @RequestMapping(value = "/token", method = RequestMethod.GET)
-    public ModelAndView token(HttpServletRequest request) throws GlobalException {
-
-        Map<String, String> paramsMap = SsoUrlUtil.tokenParams(request) ;
-
-        DataUserInfoModel user = new DataUserInfoModel() ;
-        if(paramsMap != null && !paramsMap.isEmpty()) {
-            // ssoId
-            if(paramsMap.containsKey(SsoConstants.SSO_COOKIE_NAME_USER_ID))
-                user.setSsoUserId(paramsMap.get(SsoConstants.SSO_COOKIE_NAME_USER_ID));
-            // 账号
-            if(paramsMap.containsKey(SsoConstants.SSO_COOKIE_NAME_USER_NAME))
-                user.setPhone(paramsMap.get(SsoConstants.SSO_COOKIE_NAME_USER_NAME));
-            // 头像
-            if(paramsMap.containsKey(SsoConstants.SSO_COOKIE_NAME_HEAD))
-                user.setHead(paramsMap.get(SsoConstants.SSO_COOKIE_NAME_HEAD));
-            // 昵称
-            if(paramsMap.containsKey(SsoConstants.SSO_COOKIE_NAME_NICKNAME))
-                user.setNick(paramsMap.get(SsoConstants.SSO_COOKIE_NAME_NICKNAME));
-        }
-
-        ModelAndView view = new ModelAndView(PAGE_LOGIN);
-        view.addObject(SsoConstants.SSO_USER, user) ;
-
-        return view;
-    }
-
-    /**
-     * 用户首次登陆交易平台，添加用户信息
-     * @param user
-     * @return
-     * @throws GlobalException
-     */
-    @RequestMapping(value = "/token", method = RequestMethod.POST)
-    public ModelAndView addUser(@ModelAttribute(SsoConstants.SSO_USER) DataUserInfoModel user) throws GlobalException {
-
-        if(user == null) throw new GlobalException("用户授权信息不存在，请重新授权") ;
-        // 用户类型 - 个人
-        user.setUserType(AppConstants.USER_TYPE_PERSONAL);
-        if(StringUtils.isEmpty(user.getHead())) user.setHead(null);
-
-        int id = userInfoService.insertDataUserInfo(user) ;
-        if(id < 0) throw new GlobalException("用户授权失败，请重新授权") ;
-
-        ModelAndView view = new ModelAndView();
-        view.setViewName("redirect:/callback");
-        return view;
-    }
-
-    /*@RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ModelMap registerUser(@ModelAttribute(SsoConstants.SSO_USER) DataUserInfoModel user) throws GlobalException {
-
-        Message message = new Message() ;
-        if(user == null) {
-            logger.error("用户注册失败，信息丢失");
-
-            message.setSuccess(false);
-            message.setErrorCode(ErrorCode.ERROR_REGISTER_FAIL);
-            message.setMessage("用户注册失败，信息丢失");
-        } else {
-            // 用户类型 - 个人
-            user.setUserType(AppConstants.USER_TYPE_PERSONAL);
-            if (StringUtils.isEmpty(user.getHead())) user.setHead(null);
-
-            int id = userInfoService.insertDataUserInfo(user);
-            if (id < 0) {
-                message.setSuccess(false);
-                message.setErrorCode(ErrorCode.ERROR_REGISTER_FAIL);
-                message.setMessage("用户注册失败，请重新注册");
-            } else {
-                message.setSuccess(true);
-                message.setData(user.getId());
-            }
-        }
-        ModelMap map = new ModelMap();
-        map.put("result", message) ;
-
-        return map ;
-    }*/
-
 }
