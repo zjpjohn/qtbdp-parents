11be1b6135c87f1a7ebeda6d8b8a2493890b9fdb
liyang <18758009334@163.com>
2017-06-23 16:44:45 +0800

    新增数据条数字段

---
+++ qt-trading-web/src/main/java/com/qtdbp/trading/controller/ApiDemandController.java
@@ -0,0 +1,19 @@
+package com.qtdbp.trading.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by dell on 2017/6/23.
+ */
+@Controller
+public class ApiDemandController {
+
+    private static final String ADD_API_PAGE = "apicustom/index";
+
+    @RequestMapping(value = "/add/api", method = RequestMethod.GET)
+    public String addApi() {
+        return ADD_API_PAGE;
+    }
+}
